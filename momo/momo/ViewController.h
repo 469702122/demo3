c0992d14e029ab8170dafcbd746c9ac3bb003edc
周小帅 <zhouxiaoshuai@zhouxiaoshuaidemacbook-air.local>
2016-12-01 16:43:45 +0800

    初始化处理

---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by 周小帅 on 16/12/1.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
